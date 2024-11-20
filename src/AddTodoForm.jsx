9de9ad7d8de22978d463c758d99241e83561296d
MuzhdaN <muzhda.noorzad4@gmail.com>
2024-11-19 22:31:16 -0800

    add form submission to handle AddTodoForm

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,9 +1,20 @@
-function AddTodoForm() {
+function AddTodoForm(props) {
+
+    function handleAddTodo(event) {
+        event.preventDefault();
+
+        const todoTitle = event.target['title'].value;
+        console.log(todoTitle);
+
+        props.onAddTodo(todoTitle);
+        event.target.reset()
+    };
+
     return (
-        <form>
+        <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle"> Title </label>
-            <input type="text" id="todoTitle"/>
-            <button type="submit"> Add </button>
+            <input type="text" id="todoTitle" name="title"/>
+            <button type="submit" > Add </button>
         </form>
     )
 }
