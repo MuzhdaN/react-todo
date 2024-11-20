e1cacf3b753226cde82bcd80372ecd16918baa83
MuzhdaN <muzhda.noorzad4@gmail.com>
2024-11-19 22:29:10 -0800

    store new todo in react state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react'
 import AddTodoForm from './AddTodoForm'
 import './App.css'
 import TodoList from './TodoList'
 
 function App() {
+
+  const [newTodo, setNewTodo] = useState();
+  
   return (
     <>
       <h1> Todo List </h1>
-      <AddTodoForm />
+      <AddTodoForm 
+        onAddTodo={setNewTodo}
+      />
+      <p> {newTodo} </p>
       <TodoList />
     </>
   )
