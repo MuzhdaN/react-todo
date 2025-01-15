7d6de16f99b420e8e31c2b3ed96a71641b29a9e1
MuzhdaN <muzhda.noorzad4@gmail.com>
2025-01-14 16:58:00 -0800

    Revert "Merge pull request #6 from MuzhdaN/lesson_1.5"

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import AddTodoForm from './AddTodoForm'
 import './App.css'
 import TodoList from './TodoList'
 
-function useSemiPersistentState(){
-
-  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem('savedTodoList')));
-
-  useEffect(() => {
-
-   localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-
-  }, [todoList]);
-
-  return [todoList, setTodoList]
-}
-
 function App() {
 
-  const [todoList, setTodoList] = useSemiPersistentState();
+  const [todoList, setTodoList] = useState([]);
 
   function addTodo(newTodo) {
     setTodoList([...todoList, newTodo]);
