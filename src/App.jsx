b972a35f9157bbb292330139ba5db2645f56b756
MuzhdaN <muzhda.noorzad4@gmail.com>
2025-01-19 23:26:57 -0800

    update data fetching and add loading state

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,24 @@ import TodoList from './TodoList'
 
 function App() {
 
-  const [todoList, setTodoList] = useState(JSON.parse(localStorage.getItem('savedTodoList')) || []);
+  const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-
-   localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ data: { todoList: JSON.parse(localStorage.getItem('savedTodoList')) || [] } }); 
+      }, 2000);
+    }).then((result) => {
+      setTodoList(result.data.todoList);
+      setIsLoading(false);
+    });
+  },[]);
+ 
+  useEffect(() => {
+    if (!isLoading) {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+    }
   }, [todoList]);
 
   function addTodo(newTodo) {
@@ -24,14 +36,18 @@ function App() {
   
   return (
     <>
+
       <h1> Todo List </h1>
       <AddTodoForm 
         onAddTodo={addTodo}
       />
-      <TodoList
-        todoList={todoList}
-        onRemoveTodo={removeTodo}
-      />
+      {isLoading ? (<p>Loading...</p>) 
+      : (
+        <TodoList
+          todoList={todoList}
+          onRemoveTodo={removeTodo}
+        />
+      )}
     </>
   )
 }
